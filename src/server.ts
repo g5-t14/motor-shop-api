7275f02a7c08941a7ebb74ebeb76bfb3d8ccc0ee
hermlandim <hermerson_landim@hotmail.com>
2023-06-06 15:29:26 -0300

    feat:refatoring pastes of database

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,9 @@
-// import app from "./app";
-// import "dotenv/config"
-// import { AppDataSource } from "./data-source";
-
-
-// AppDataSource.initialize()
-//     .then(() => {
-//         console.log("Database running");
-        // const PORT = process.env.PORT || 3000
-        // app.listen(PORT, () => {
-        //     console.log(`Server is running on PORT ${PORT}`);
-        // })
-//     })
-//     .catch(error => console.log(error))
-
 import { PrismaClient } from '@prisma/client'
 import app from './app';
 
 const prisma = new PrismaClient({ log: ["info", "query", "warn", "error"] })
 
-prisma.$connect()
-    .then(() => {
-        const PORT = process.env.PORT || 3000
-        app.listen(PORT, () => {
-            console.log(`Server is running on PORT ${PORT}`);
-        })
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-  })
+app.listen(3000, () => console.log("server is running"))
 
 
 export { prisma }
