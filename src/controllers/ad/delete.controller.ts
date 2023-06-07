4020bb14464c6505fd2f4c7e427654c89b4de815
Jozhué Beni <jbeeniif@gmail.com>
2023-06-07 14:14:41 -0300

    fix: fixed bugs on delete Route

--- src/controllers/ad/delete.controller.ts
+++ src/controllers/ad/delete.controller.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { deleteAdService } from "../../services/ad/delete.service";
 
 export const deleteAdController = async (req: Request, res: Response) => {
-  const userId: number = parseInt(req.params.id);
+  const adId: number = parseInt(req.params.id);
 
-  const deleteAd = await deleteAdService(userId);
+  const deleteAd = await deleteAdService(adId);
 
   return res.json(deleteAd);
 };
