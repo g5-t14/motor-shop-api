2f8624336c045c1e02b7448cb701b44ae81175df
Victor Guterres <Vgb1337@gmail.com>
2023-06-17 12:40:22 -0300

    finished:reset_password

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -9,7 +9,7 @@ const userSchema = z.object({
     .string()
     .max(60)
     .transform((password) => hashSync(password, 10)),
-  reset_password: z.string().nullish(),
+  reset_password: z.string(),
   cpf: z.string().max(11),
   phone: z.string().max(11),
   birthdate: z.string().max(8),
@@ -27,6 +27,7 @@ const userSchema = z.object({
 const userSchemaRequest = userSchema.omit({
   id: true,
   user_color: true,
+  reset_password:true
 });
 
 const userSchemaColorRequest = userSchema.omit({
